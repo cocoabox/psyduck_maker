a025d5f885befce8c9071f919ff807ee96fbbf93
cocoabox <cocoa.kei+1@gmail.com>
2016-07-31 05:11:32 +0900

    画像追加。README追加

--- make_images_js.sh
+++ make_images_js.sh
@@ -4,18 +4,18 @@ echo "window.IMAGE_DATA = {" > $OUT
 COMMA=""
 
 write_file() {
+    echo "${1}.png" >&2
     BASE64=`cat "$1".png | base64`
     echo "${COMMA}'$1':'data:image/png;base64,${BASE64}'" >>$OUT
     COMMA=","
 
 }
-
-for F in {0..10}; do 
+for F in `seq 0 12`; do
     FN="${F}"
     if [ $F -lt 10 ]; then
         FN="0${F}"
-        write_file "$FN"
-    fi
+    fi 
+    write_file "$FN"
 done
 
 write_file "pencil"
